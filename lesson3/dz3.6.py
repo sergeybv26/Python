4311b5342b8a184d75bcf10a3ed610a9521ab7cf
ssukhanov <sergey.bv26@gmail.com>
2021-01-20 22:56:56 +0300

    Домашнее задание к уроку 3 выполнено

---
+++ lesson3/dz3.6.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+	def int_funct(word):
+		"""Выполняет замену первой буквы слова со строчной на прописную"""
+		first_symb = ord(word[0])
+		first_symb = first_symb - 32
+		new_word = chr(first_symb) + word[1:]
+		return new_word
+
+
+	user_word = input('Введите слово строчными латинскими буквами: ')
+	print('Преобразованное слово:', int_funct(user_word))
+
+	user_str = input('Введите строку из слов, разделенных пробелами: ').split()
+	for i in range(len(user_str)):
+		print(int_funct(user_str[i]), end=' ')
