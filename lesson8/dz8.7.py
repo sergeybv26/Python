7d9f86c3fda7a542e843fad5b9cb5476cde49ef3
sergeybv26 <77108251+sergeybv26@users.noreply.github.com>
2021-02-10 16:36:03 +0300

    Выполнено 7 задание к уроку 8
    Требуется проверка в IDE

---
+++ lesson8/dz8.7.py
@@ -0,0 +1,19 @@
+class ComplexNumber:
+    def __init__(self, complex_number):
+        self.complex_number = complex_number
+        
+    def __add__(self, other):
+        return ComplexNumber(self.complex_number + other.complex_number)
+        
+    def __mul__(self, other):
+        return ComplexNumber(self.complex_number * other.complex_number)
+        
+    def __str__(self):
+        return f'Результат выполнения операции: {self.complex_number}'
+        
+        
+cn1 = ComplexNumber(1+2j)
+cn2 = ComplexNumber(4+6j)
+
+print(cn1 + cn2)
+print(cn1 * cn2)
